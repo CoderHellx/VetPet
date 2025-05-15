281929264f722e6e5fd0acc4c3693492b0d90a35
Meryem Şeker <sekermeryem51@gmail.com>
2025-05-15 05:37:41 +0300

    sign in xml editted

--- app/src/main/java/com/example/project/SignUpActivity.java
+++ app/src/main/java/com/example/project/SignUpActivity.java
@@ -14,6 +14,7 @@ import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
